fea7c5d0259a8b0b3a1fa5007aa8c7d2469224d3
Heidi Olsen <heidi@eroi.com>
2019-09-10 22:02:32 -0700

    ✨ Update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,7 +55,7 @@ module.exports = {
       resolve: 'gatsby-plugin-react-svg',
       options: {
           rule: {
-            include: /img/
+            include: /svg/
           }
       }
     },
